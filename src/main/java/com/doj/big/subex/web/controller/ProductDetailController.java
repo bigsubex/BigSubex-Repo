6eb7b083baeca8f65b3bf8cd34c767ea262eb35a
dineshonjava <admin@dineshonjava.com>
2015-06-04 23:18:52 +0530

    changes in controllers

--- src/main/java/com/doj/big/subex/web/controller/ProductDetailController.java
+++ src/main/java/com/doj/big/subex/web/controller/ProductDetailController.java
@@ -4,11 +4,14 @@
 package com.doj.big.subex.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.doj.big.subex.domain.Product;
 import com.doj.big.subex.service.ProductDetailService;
 import com.doj.big.subex.web.utils.BigConstant;
 
@@ -25,5 +28,12 @@ public class ProductDetailController {
 	@RequestMapping(value = BigConstant.PRODUCT_DETAIL_PAGE, method = RequestMethod.GET)
 	public ModelAndView product(ModelMap model){
 		return new ModelAndView(BigConstant.PRODUCT_DETAIL);
+	}
+	
+	@RequestMapping(value = BigConstant.PRODUCT_DETAIL_PAGE+"/{productId}")
+	public String details(@PathVariable("productId") long productId, Model model) {
+		Product product = this.productDetailService.findProduct(productId);
+		model.addAttribute(product);
+		return BigConstant.PRODUCT_DETAIL;
 	}
 }
--- src/main/java/com/doj/big/subex/web/controller/ProductSearchController.java
+++ src/main/java/com/doj/big/subex/web/controller/ProductSearchController.java
@@ -3,12 +3,16 @@
  */
 package com.doj.big.subex.web.controller;
 
+import java.util.Collection;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.doj.big.subex.domain.Product;
+import com.doj.big.subex.domain.ProductSearchCriteria;
 import com.doj.big.subex.service.ProductSearchService;
 import com.doj.big.subex.web.utils.BigConstant;
 
@@ -26,5 +30,9 @@ public class ProductSearchController {
 	public ModelAndView searchProduct(ModelMap model){
 		return new ModelAndView(BigConstant.PRODUCT_SEARCH);
 	}
-
+	
+	@RequestMapping(value = BigConstant.PRODUCT_SEARCH_PAGE, method = RequestMethod.POST)
+	public Collection<Product> list(ProductSearchCriteria criteria ) {
+		return this.productSearchService.findProducts(criteria);
+	}
 }
